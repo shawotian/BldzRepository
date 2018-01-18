b77563ad19cffddb9c3ee2861d3a2e4e79193f1d
YLY <tianxiaoxiao@bldz.com>
2018-01-18 14:46:36 +0800

    ssads

--- Example/BldzPodLIb/prefixWhatViewController.h
+++ Example/BldzPodLIb/prefixWhatViewController.h
@@ -10,4 +10,6 @@
 
 @interface prefixWhatViewController : UIViewController
 
+@property(nonatomic,strong)NSString *testStr;
+
 @end
---
+++ Example/Pods/Target Support Files/BldzPodLIb/BldzPodLIb-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BldzPodLIb : NSObject
+@end
+@implementation PodsDummy_BldzPodLIb
+@end
